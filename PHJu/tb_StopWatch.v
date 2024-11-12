d1d4c944cf5b0c3310ba9298b8b3d05a830e7856
hyeon <hyeonju0925@naver.com>
2024-11-13 02:38:02 +0900

    update code

--- PHJu/tb_StopWatch.v
+++ PHJu/tb_StopWatch.v
@@ -2,10 +2,13 @@
 module tb_StopWatch;
     reg Clk;
     reg Rst;
+    reg fRecord;
     reg fStart;
     reg fStop;
+    wire [6:0] o_Sec0, o_Sec1, o_Sec2, o_Sec3, o_Sec4, o_Sec5;
 
-    StopWatch U0(Clk, Rst, fStart, fStop, , , );
+
+    StopWatch U0(Clk, Rst, fRecord, fStart, fStop, o_Sec0, o_Sec1, o_Sec2, o_Sec3, o_Sec4, o_Sec5);
     
     always #10 Clk = ~Clk;
     
@@ -15,14 +18,16 @@ module tb_StopWatch;
         Rst = 0;
         fStart = 1;
         fStop = 1;
+        fRecord =1;
         
         @(negedge Clk) Rst = 1;
+        #100 fStart = 0; #20 fStart = 1;
+
+        #500 fRecord = 0; #20 fRecord = 1;
+
         #1000 fStart = 0; #20 fStart = 1;
-        #222_222_200 fStart = 0; #20 fStart = 1;
-        #222_222_200 fStart = 0; #20 fStart = 1;
-        #222_222_200 fStart = 0; #20 fStart = 1;
-        #222_222_200 fStart = 0; #20 fStart = 1;
-        #333 fStop = 0; #20 fStop = 1;
+
+        #500 fStop = 0; #20 fStop = 1;
         $stop;
     end
 endmodule
