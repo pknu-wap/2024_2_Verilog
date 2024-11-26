755b51e0a2f77e91f8d35333f672099d0f82b42a
ParkHyeon89 <parkhyeon0809@gmail.com>
2024-11-26 22:46:09 +0900

    [박현]Player_Position

--- game_team/code/Player_Position.v
+++ game_team/code/Player_Position.v
@@ -1,6 +1,5 @@
 module Player_Position (i_Btn_Left,i_Btn_Right,i_Player_Posion
                         ,i_fTick, o_Player_Position);
-                        
 
     input i_Btn_Left;
     input i_Btn_Right;
@@ -8,9 +7,16 @@ module Player_Position (i_Btn_Left,i_Btn_Right,i_Player_Posion
     input i_fTick;
     output reg [9:0] o_PlayerPosition;
     
+    parameter MONITOR_WIDTH     = 10'd640;
+    parameter PLAYER_WIDTH      = 10'd24;
+
+    wire fPlayerLeftTouch, fPlayerRightTouch;
+
+    assign fPlayerLeftTouch = ~|i_PlayerPosition;
+    assign fPlayerRightTouch = i_PlayerPosition == MONITOR_WIDTH - PLAYER_WIDTH;
+
     assign o_PlayerPosition = 
-        (i_Btn_Left  && i_PlayerPosition > 10'd0)             ? (i_PlayerPosition - 10'd1) :
-        (i_Btn_Right && i_PlayerPosition < (10'd240 - 10'd24)) ? (i_PlayerPosition + 10'd1) :
-                                                                i_PlayerPosition;
+        (i_Btn_Left  & ~fPlayerLeftTouch)   ? (i_PlayerPosition - 10'd1) :
+        (i_Btn_Right & ~fPlayerRightTouch)  ? (i_PlayerPosition + 10'd1) : i_PlayerPosition;
 
 endmodule
