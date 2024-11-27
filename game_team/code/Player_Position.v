611ed84863d5036de18a770eb9ffa5fd92180fe9
ParkHyeon89 <parkhyeon0809@gmail.com>
2024-11-27 21:26:58 +0900

    [박현] Player_Position & tb_Player_Position
    Player Position Test Bench

--- game_team/code/Player_Position.v
+++ game_team/code/Player_Position.v
@@ -1,22 +1,23 @@
-module Player_Position (i_Btn_Left,i_Btn_Right,i_Player_Posion
+module Player_Position (i_Clk, i_Rst, i_Btn_Left, i_Btn_Right, i_Player_Position
                         ,i_fTick, o_Player_Position);
 
+    input i_Clk, i_Rst;
     input i_Btn_Left;
     input i_Btn_Right;
     input [9:0] i_Player_Position;
     input i_fTick;
-    output reg [9:0] o_PlayerPosition;
+    output [9:0] o_Player_Position;
     
     parameter MONITOR_WIDTH     = 10'd640;
     parameter PLAYER_WIDTH      = 10'd24;
 
     wire fPlayerLeftTouch, fPlayerRightTouch;
 
-    assign fPlayerLeftTouch = ~|i_PlayerPosition;
-    assign fPlayerRightTouch = i_PlayerPosition == MONITOR_WIDTH - PLAYER_WIDTH;
+    assign fPlayerLeftTouch = ~|i_Player_Position;
+    assign fPlayerRightTouch = i_Player_Position == MONITOR_WIDTH - PLAYER_WIDTH;
 
-    assign o_PlayerPosition = 
-        (i_Btn_Left  & ~fPlayerLeftTouch)   ? (i_PlayerPosition - 10'd1) :
-        (i_Btn_Right & ~fPlayerRightTouch)  ? (i_PlayerPosition + 10'd1) : i_PlayerPosition;
+    assign o_Player_Position = 
+        (i_Btn_Left  & ~fPlayerLeftTouch)   ? (i_Player_Position - 10'd1) :
+        (i_Btn_Right & ~fPlayerRightTouch)  ? (i_Player_Position + 10'd1) : i_Player_Position;
 
 endmodule
