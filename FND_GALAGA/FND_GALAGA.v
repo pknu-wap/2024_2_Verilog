7791cfef03fda5b5f25679214a892937effb02ab
ParkHyeon89 <parkhyeon0809@gmail.com>
2024-11-28 15:28:42 +0900

    [박현] FND_GALAGA 수정

--- FND_GALAGA/FND_GALAGA.v
+++ FND_GALAGA/FND_GALAGA.v
@@ -31,31 +31,57 @@ module FND_GALAGA(i_Clk, i_Rst, i_fGameStartStop,
     FND FND1(c_Sec1, o_Sec1);
     FND FND2(c_Sec2, o_Sec2);
 
-    if (n_ClkCnt >= LST_CLK) begin
-        n_ClkCnt = 0;
+     always@(posedge i_Clk, negedge i_Rst) //??? ?? ? 0
+    
+        if(!i_Rst) begin
+            c_State = GAME_IDLE;
+            c_ClkCnt = 0;
+            c_Sec0 = 0;
+            c_Sec1 = 0;
+            c_Sec2 = 0; 
+            c_fStart = 1;
+            c_fStop = 1;
+        end else begin
+            c_State = n_State ;
+            c_ClkCnt = n_ClkCnt ;
+            c_Sec0 = n_Sec0 ;
+            c_Sec1 = n_Sec1 ;
+            c_Sec2 = n_Sec2 ;
+            c_fStart = n_fStart ;
+            c_fStop = n_fStop ;
+        end 
 
-        if (c_GameStartStop == GAME_PLAYING) begin
-                
-                if (c_Sec0 == 9) begin
-                    n_Sec0 = 0;
+        assign fStart = !i_fStart && c_fStart,
+               fStop = !i_fStop && c_fStop;
+        
+        assign fLstClk = c_ClkCnt == LST_CLK,
+               fLstSec0= c_Sec0 == 9,
+               fLstSec1= c_Sec1 == 9,
+               fLstSec2= c_Sec2 == 9;
 
-                    if (c_Sec1 == 9) begin
-                        n_Sec1 = 0;
+        assign fIncSec0= fLstClk,
+               fIncSec1= fIncSec0 && fLstSec0,
+               fIncSec2= fIncSec1 && fLstSec1;
 
-                        if (c_Sec2 == 9) begin
-                            n_Sec2 = 0;
-                        end else begin
-                            n_Sec2 = c_Sec2 + 1;
-                        end
-                    end else begin
-                        n_Sec1 = c_Sec1 + 1;
-                    end
-                end else begin
-                    n_Sec0 = c_Sec0 + 1;
-                end
+        case(c_GameStartStop)
+            GAME_IDLE: begin
+                n_ClkCnt = 0;
+                n_Sec0 = 0;
+                n_Sec1 = 0;
+                n_Sec2 = 0;
+                if(fStart) n_State = WORK;
             end
-        end
-    end
-
-
+            GAME_PLAYING: begin
+                n_ClkCnt = fLstClk ? 0 : c_ClkCnt + 1;
+                n_Sec0 = fIncSec0 ? fLstSec0 ? 0 : c_Sec0 + 1 : c_Sec0;
+                n_Sec1 = fIncSec1 ? fLstSec1 ? 0 : c_Sec1 + 1 : c_Sec1;
+                n_Sec2 = fIncSec2 ? fLstSec2 ? 0 : c_Sec2 + 1 : c_Sec2;
+                if(fStop) n_State = IDLE;
+                else if(fStart)n_State = PAUSE;
+            end
+            GAME_INITIAL: begin
+                if(fStop) n_State = IDLE;
+                else if(fStart)n_State = WORK;
+            end
+        endcase
 endmodule
