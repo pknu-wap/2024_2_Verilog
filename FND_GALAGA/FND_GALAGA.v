c13e81d0546b07b82daa3c587cdf97739e84263c
ParkHyeon89 <parkhyeon0809@gmail.com>
2024-11-28 22:11:49 +0900

    [박현]FND_GALAGA.v 수정_2

--- FND_GALAGA/FND_GALAGA.v
+++ FND_GALAGA/FND_GALAGA.v
@@ -1,87 +1,98 @@
-module FND_GALAGA(i_Clk, i_Rst, i_fGameStartStop,
-				  o_Sec0, o_Sec1, o_Sec2,);
+module FND_GALAGA(i_Clk, i_Rst, 
+                  i_GameStartStop, i_Score,
+                  o_Sec0, o_Sec1,o_Sec2, o_Score, 
+                  i_Num, o_FND);
 
-	input i_Clk, i_Rst;
-	input i_fGameStartStop;
-	ouput wire [6:0] o_Sec0, o_Sec1, o_Sec2;
+    input [3:0] i_Num;
+    output reg [6:0] o_FND;
+ 
+    input wire        i_Clk;
+    input wire        i_Rst;
 
-	parameter LST_CLK = 100_000_000/20 - 1;
-    parameter IDLE = 2'b00, WORK = 2'b01, Stop = 2'b10;
+    input wire        i_GameStartStop;
+    input wire [13:0] i_Score;
 
-	reg [1:0] c_GameStartStop,n_GameStartStop;
+    output reg [6:0]  o_Sec0, o_Sec1, o_Sec2;
+    output reg [13:0] o_Score;
 
-	reg [3:0] c_Sec0, n_Sec0;
-    reg [3:0] c_Sec1, n_Sec1;
-	reg [3:0] c_Sec2, n_Sec2;
+    parameter LST_CLK = 100_000_000 - 1; 
+    reg [22:0] c_ClkCnt, n_clkCnt;
 
-	reg [22:0] c_ClkCnt, n_ClkCnt;
-    reg c_fGameStartStop, n_fGameStartStop;
+    reg [3:0]  c_Sec0, c_Sec1, c_Sec2;
 
-	wire fLstClk;
-    reg c_fStart, n_fStart;
-    reg c_fStop, n_fStop;
-
-    wire fLstSec0, fLstSec1, fLstSec2;
-	wire fIncSec0, fIncSec1, fIncSec2;
-
-	wire fGameStartStop;
-	wire fStart, fStop;
-	
-	FND FND0(c_Sec0, o_Sec0);
-    FND FND1(c_Sec1, o_Sec1);
-    FND FND2(c_Sec2, o_Sec2);
-
-     always@(posedge i_Clk, negedge i_Rst) //??? ?? ? 0
+    reg        c_GamePlaying;
     
-        if(!i_Rst) begin
-            c_State = GAME_IDLE;
-            c_ClkCnt = 0;
-            c_Sec0 = 0;
-            c_Sec1 = 0;
-            c_Sec2 = 0; 
-            c_fStart = 1;
-            c_fStop = 1;
+    always @(posedge i_Clk or negedge i_Rst) begin
+        if (!i_Rst) begin
+            c_ClkCnt <= 0;
+            c_Sec0 <= 0;
+            c_Sec1 <= 0;
+            c_Sec2 <= 0;
+            c_GamePlaying <= 0;
+            o_Score <= 0;
+
         end else begin
-            c_State = n_State ;
-            c_ClkCnt = n_ClkCnt ;
-            c_Sec0 = n_Sec0 ;
-            c_Sec1 = n_Sec1 ;
-            c_Sec2 = n_Sec2 ;
-            c_fStart = n_fStart ;
-            c_fStop = n_fStop ;
-        end 
-
-        assign fStart = !i_fStart && c_fStart,
-               fStop = !i_fStop && c_fStop;
-        
-        assign fLstClk = c_ClkCnt == LST_CLK,
-               fLstSec0= c_Sec0 == 9,
-               fLstSec1= c_Sec1 == 9,
-               fLstSec2= c_Sec2 == 9;
-
-        assign fIncSec0= fLstClk,
-               fIncSec1= fIncSec0 && fLstSec0,
-               fIncSec2= fIncSec1 && fLstSec1;
-
-        case(c_GameStartStop)
-            GAME_IDLE: begin
-                n_ClkCnt = 0;
-                n_Sec0 = 0;
-                n_Sec1 = 0;
-                n_Sec2 = 0;
-                if(fStart) n_State = WORK;
+            if (i_GameStartStop) begin
+                c_GamePlaying <= 1;
             end
-            GAME_PLAYING: begin
-                n_ClkCnt = fLstClk ? 0 : c_ClkCnt + 1;
-                n_Sec0 = fIncSec0 ? fLstSec0 ? 0 : c_Sec0 + 1 : c_Sec0;
-                n_Sec1 = fIncSec1 ? fLstSec1 ? 0 : c_Sec1 + 1 : c_Sec1;
-                n_Sec2 = fIncSec2 ? fLstSec2 ? 0 : c_Sec2 + 1 : c_Sec2;
-                if(fStop) n_State = IDLE;
-                else if(fStart)n_State = PAUSE;
+            
+            if (c_GamePlaying) begin
+                if (c_ClkCnt >= LST_CLK) begin
+                    c_ClkCnt <= 0;
+
+                    if (c_Sec0 == 9) begin
+                        c_Sec0 <= 0;
+
+                        if (c_Sec1 == 9) begin
+                            c_Sec1 <= 0;
+
+                            if (c_Sec2 == 9) begin
+                                c_Sec2 <= 0;
+
+                            end else begin
+                                c_Sec2 <= c_Sec2 + 1;
+                            end
+
+                        end else begin
+                            c_Sec1 <= c_Sec1 + 1;
+                        end
+
+                    end else begin
+                        c_Sec0 <= c_Sec0 + 1;
+                    end
+
+                    o_Score <= i_Score;
+
+                end else begin
+                    c_ClkCnt <= c_ClkCnt + 1;
+                end
+
+            end else begin
+                c_GamePlaying <= i_GameStartStop;
             end
-            GAME_INITIAL: begin
-                if(fStop) n_State = IDLE;
-                else if(fStart)n_State = WORK;
-            end
-        endcase
+
+        end
+    end
+
+    always @* begin
+        o_Sec0 = c_Sec0;
+        o_Sec1 = c_Sec1;
+        o_Sec2 = c_Sec2;
+    end
+
+    always@*
+        case (i_Num)
+            4'h0: o_FND = 7'b1000000;
+            4'h1: o_FND = 7'b1111001;
+            4'h2: o_FND = 7'b0100100;
+            4'h3: o_FND = 7'b0110000;
+            4'h4: o_FND = 7'b0011001;
+            4'h5: o_FND = 7'b0010010;
+            4'h6: o_FND = 7'b0000010;
+            4'h7: o_FND = 7'b1111000;
+            4'h8: o_FND = 7'b0000000;
+            4'h9: o_FND = 7'b0010000;
+    endcase
+
+
 endmodule
